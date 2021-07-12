7bbd220bb60146f54a5a4c4c210f8a13604c41a0
Kbz <kilezisme@gmail.com>
2021-07-12 21:51:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+for i in range(10):
+  print(i)
+
+var1 = 6
+var2 = "Test"
+var3 = 3
+
+print("\n")
+print(var1)
+print("Addition", var1 + var3)
+print("Division: ", var1 / var3)
+print(var2)
